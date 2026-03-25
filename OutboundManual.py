ca4e603c9780fa9da9d3d6f300971282c67ad339
Salvador Alejandro Sanchez Silva <salvador.s@maersk.com>
2026-03-25 08:10:44 -0600

    Endpoints update

--- OutboundManual.py
+++ OutboundManual.py
@@ -29,7 +29,7 @@ ENDPOINTS = {
 # ===========================================
 
 PGHOST = os.getenv("PGHOST", "192.168.25.48")
-PGPORT = os.getenv("PGPORT", "5432")
+PGPORT = os.getenv("PGPORT", "2345")
 PGDATABASE = os.getenv("PGDATABASE", "mcs-onr-db")
 PGUSER = os.getenv("PGUSER", "postgres")
 PGPASSWORD = os.getenv("PGPASSWORD", "postgres")
